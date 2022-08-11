22a51b043bded392b2778dac115c498e490f1659
Mustapha-Nawawi-T <nawawimstapha@gmail.com>
2022-08-11 16:40:03 +0100

    +

--- Makefile
+++ Makefile
@@ -1,9 +1,9 @@
 
 all:
-	cd srcs && sudo docker-compose up --build
+	cd srcs && sudo docker-compose up --build -d
 
 clean :
-	sudo docker rm $$(sudo docker ps -aq)
+	sudo docker rm -f $$(sudo docker ps -aq)
 
 fclean: clean
 	sudo docker rmi $$(sudo docker images -q)
--- srcs/requirements/nginx/tools/run_nginx.sh
+++ srcs/requirements/nginx/tools/run_nginx.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 
-envsubst < default.templat > default.conf
+envsubst < conf/default.templat > conf/default.conf
 
-mkdir /etc/nginx/ssl
+rm -rf conf/default.templat
+
+mv conf/nginx.conf .
+
+mkdir ssl
 
 openssl req -x509 -nodes -days 1 \
-	-subj "/CN=${DOMAIN_NAME} www.${DOMAIN_NAME}" \
+	-subj "/C=MA/ST=Marrakesh-Safi/L=Ben-Guerir/O=42-network/OU=1337/CN=${DOMAIN_NAME} www.${DOMAIN_NAME}" \
 	-newkey rsa:2048 -keyout /etc/nginx/ssl/nginx-selfsigned.key \
 	-out /etc/nginx/ssl/nginx-selfsigned.crt
 
