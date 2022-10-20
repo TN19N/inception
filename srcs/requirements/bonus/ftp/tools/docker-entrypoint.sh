18e419cd6d88e61fb93808a8cdb640e4117b4a38
Mustapha-Nawawi-T <nawawimstapha@gmail.com>
2022-10-20 12:06:17 +0100

    back Up

--- srcs/requirements/bonus/ftp/tools/docker-entrypoint.sh
+++ srcs/requirements/bonus/ftp/tools/docker-entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 set -e
 
-adduser -H ${FTP_USER_NAME} << EOF
+adduser -h /var/www/wordpress ${FTP_USER_NAME} << EOF
 ${FTP_USER_PASSWORD}
 ${FTP_USER_PASSWORD}
 EOF
 
-echo ${FTP_USER_NAME} >> vsftpd.userlist
+echo ${FTP_USER_NAME} > vsftpd.userlist
 
 rm -f docker-entrypoint.sh
 exec "$@"
