ec574030311c23c727c205c509ae722efadaa9c7
Mustapha-Nawawi-T <nawawimstapha@gmail.com>
2022-10-21 11:52:24 +0100

    back Up

--- srcs/requirements/bonus/ftp/tools/docker-entrypoint.sh
+++ srcs/requirements/bonus/ftp/tools/docker-entrypoint.sh
@@ -6,7 +6,10 @@ ${FTP_USER_PASSWORD}
 ${FTP_USER_PASSWORD}
 EOF
 
+chown -R ${FTP_USER_NAME}:${FTP_USER_NAME} /var/www
+
 echo ${FTP_USER_NAME} > vsftpd.userlist
+
 
 rm -f docker-entrypoint.sh
 exec "$@"
